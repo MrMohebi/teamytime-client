169635f567de28b0f9e2bffc0e042c1debb4ad10
MMMoehebi <MMMohebi@outlook.com>
2022-03-15 11:06:57 +0330

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary': '#0099ff',
+      'primary-light': '#f1f5f8',
+      'default': '#f0f2f5',
+      'white' : '#fefeff',
+    },
   },
   plugins: [],
 }
