2da92d99d83ab6e1b4383154c2388bca8032b19e
Mokafela <mkd0098@gmail.com>
2022-08-01 15:11:15 +0430

    deploy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
             <Head>
                 <title>ساعت کاری</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name='theme-color' content={"#1D2731"}/>
 
             </Head>
             <body>
