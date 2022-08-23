00f3312bd2a0111fcd258bbe4509ce839099d4cd
Mokafela <mkd0098@gmail.com>
2022-08-23 16:31:02 +0430

    deploy

--- pages/error.tsx
+++ pages/error.tsx
@@ -6,6 +6,11 @@ const Error = () => {
             <button className={'w-44 h-20 text-white rounded-2xl shadow m-auto bg-primary'} onClick={() => {
                 console.error('shit,s error')
 
+                let a =1;
+
+                // @ts-ignore
+                a.split("d");
+
             }}> Click to error
             </button>
 
