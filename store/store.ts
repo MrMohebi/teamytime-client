aba1034be62f778d29c51479568312916834ff7c
Mokafela <mkd0098@gmail.com>
2023-02-19 21:16:24 +0330

    prod-deploy +company name fixed

--- store/store.ts
+++ store/store.ts
@@ -1,18 +1,18 @@
 import {makeVar} from "@apollo/client";
 
 
-export const CompanyName = makeVar("")
-export const CompanyId = makeVar("")
-export const CompanyTextFields = makeVar([] as any[])
-export const CompanyTimeFields = makeVar([] as any[])
-export const UserId = makeVar("")
-export const AdminID = makeVar("")
-export const CurrentSelectedDate = makeVar("")
-export const UserLocalDays = makeVar({} as any)
-export const CompanyRequiredFields = makeVar([] as any)
-export const BaseURL = makeVar("https://timeservice.unimun.me/")
-export const CoolDown = makeVar(false)
-export const CurrentDay = makeVar("")
-export const CollapseHeader = makeVar(false)
-export const StartDate = makeVar("")
-export const CompanyAllowedDays = makeVar(0)
+export const CompanyName = makeVar("");
+export const CompanyId = makeVar("");
+export const CompanyTextFields = makeVar([] as any[]);
+export const CompanyTimeFields = makeVar([] as any[]);
+export const UserId = makeVar("");
+export const AdminID = makeVar("");
+export const CurrentSelectedDate = makeVar("");
+export const UserLocalDays = makeVar({} as any);
+export const CompanyRequiredFields = makeVar([] as any);
+export const BaseURL = makeVar("https://timeservice.unimun.me/");
+export const CoolDown = makeVar(false);
+export const CurrentDay = makeVar("");
+export const CollapseHeader = makeVar(false);
+export const StartDate = makeVar("");
+export const CompanyAllowedDays = makeVar(0);
